90f9ae5f79802c4cf821591103724bb2b4879c49
erikth97 <erik.tamayohe@uanl.edu.mx>
2023-07-12 10:38:37 -0600

    deletaDataModels

--- modularizacion/comentarios/views.py
+++ modularizacion/comentarios/views.py
@@ -1,9 +1,19 @@
+
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Comment
 
 def test(request):
     return HttpResponse("Funciona el envio de datos")
     
 def create(request):
+    # comment = Comment(name="ErikT", score=5, comment="Este es un comentario")
+    # comment.save()
+    comment = Comment.objects.create(name="Fabian")
     return HttpResponse("creacion de modelos")
+
+def delete(request):
+    # comment = Comment.objects.get(id=2)
+    # comment.delete()
+    Comment.objects.filter(id=3).delete()
+    return HttpResponse("Ruta para probar los delete")
