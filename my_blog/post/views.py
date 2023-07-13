1e5007f16aa4f7affb6765db7948060e2536905c
erikth97 <erik.tamayohe@uanl.edu.mx>
2023-07-13 12:28:17 -0600

    dataQueries

--- my_blog/post/views.py
+++ my_blog/post/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from .models import Author, Entry
 
-# Create your views here.
+def queries(request):
+    # Obtener todos los elementos
+    authors = Author.objects.all()
+
+    # Obtener datos diltrados por condicion
+    filtered = Author.objects.filter(email='awalker@example.org')
+
+    # Obtener un unico objeto (filtrado)
+    author = Author.objects.get(id=1)
+
+    # Obtener los 10 primeros elementos
+    limits = Author.objects.all()[:10]
+
+    # Obtener 5 elementos saltando los 5 primeros
+    offsets = Author.objects.all()[5:10]
+    
+    return render(request, 'post/queries.html', {'authors': authors, 'filtered': filtered, 'author' : author, 'limits' : limits, 'offsets' : offsets})
