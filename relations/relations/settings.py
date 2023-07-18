9ffab86743254153baa5dc824b479d867760f159
erikth97 <erik.tamayohe@uanl.edu.mx>
2023-07-18 12:08:58 -0600

    postgresqlConect

--- relations/relations/settings.py
+++ relations/relations/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'relations.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql1',
+        'NAME': 'django_sample',
+        'USER': 'postgres',
+        'PASSWORD': 'densoPassword',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
