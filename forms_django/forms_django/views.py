029201b815fa5c8db7dde845b4397371879ceac4
erikth97 <erik.tamayohe@uanl.edu.mx>
2023-07-21 11:18:19 -0600

    validationForm

--- forms_django/forms_django/views.py
+++ forms_django/forms_django/views.py
@@ -10,8 +10,19 @@ def goal(request):
     if request.method != 'POST':
         return HttpResponse('Metodo no permitido')
     
+
     return HttpResponse(request.POST['name'])
 
 def widget(request):
-    form = CommentForm()
-    return render(request, 'widget.html', {'form' : form})
+    if request.method == 'GET':
+       form = ContactForm()
+       return render(request, 'widget.html', {'form' : form})
+    
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            # Aqui irian todas las acciones a realizar cuando los datos son correctos
+            return HttpResponse("Valido")
+        else:
+            # Aqui comunicamos al usuario que los datos no son validos
+            return render(request, 'widget.html', {'form' : form})
