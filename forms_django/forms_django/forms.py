2ddb5351c356f15a60153d6b5a28b4bae0107967
erikth97 <erik.tamayohe@uanl.edu.mx>
2023-07-21 11:18:14 -0600

    validationForm

--- forms_django/forms_django/forms.py
+++ forms_django/forms_django/forms.py
@@ -8,4 +8,11 @@ class CommentForm(forms.Form):
 class ContactForm(forms.Form):
     name = forms.CharField(label="Nombre:", max_length=50, widget=forms.TextInput(attrs={'class': 'form-control'}))
     email = forms.EmailField(label="Email", max_length=50, widget=forms.EmailInput(attrs={'class': 'form-control'}))
-    message = forms.CharField(label="Mensaje",widget=forms.Textarea(attrs={'class': 'form-control'}))    
+    message = forms.CharField(label="Mensaje", widget=forms.Textarea(attrs={'class': 'form-control'}))  
+
+    def clean_name(self):
+        name = self.cleaned_data.get("name")
+        if name != "Erik":
+            raise forms.ValidationError("Tan solo el valor Erik esta permitido para este campo")
+        else:
+            return name  
